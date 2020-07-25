753a2e0b2726526a56539095d7c5114d6f4dbe4e
Max Carvajal <jcarvajal@crimson.ua.edu>
2020-07-25 14:22:31 -0500

    construct scryfall image ids

--- src/core/card_database.js
+++ src/core/card_database.js
@@ -11,13 +11,56 @@ async function fetch_set(set_code) {
     filename: dbPath,
     driver: sqlite3.Database
   }).then(async(db) => {
+    //const query = 'SELECT * FROM cards WHERE name = "Alchemist\'s Gift"'
     const query = 'SELECT * FROM cards WHERE setCode = "' + set_code + '"'
-    return await db.get(query);
+    return await db.all(query);
   })
 }
 
 export function fetch_cards_for_set(set_code) {
-  return fetch_set(set_code)
+  return fetch_set(set_code).then((cards) => {
+    return cards.filter(remove_promos).map((card) => {
+      return {
+        id: card.multiverseId,
+        name: card.name,
+        collector_number: card.number,
+        multiverse_ids: [card.multiverseId],
+        image_uris: [construct_image_uri(card.scryfallId)],
+        layout: card.layout,
+        type_line: card.type,
+        oracle_text: card.text,
+        mana_cost: card.manaCost,
+        cmc: card.convertedManaCost,
+        colors: card.colors,
+        latent_color: fetch_latent_color(),
+        rarity: card.rarity,
+        rating: fetch_rating(card.multiverseId),
+        set: set_code.toLowerCase()
+      };
+    });
+  });
+}
+
+function construct_image_uri(scryfall_id) {
+  const char0 = scryfall_id.charAt(0)
+  const char1 = scryfall_id.charAt(1)
+  return `https://img.scryfall.com/cards/normal/front/${char0}/${char1}/${scryfall_id}.jpg`
+}
+
+function remove_promos(card) {
+  // TODO: filter out if 'promoTypes' field exists
+  // TODO: This doesn't work!
+  return card.hasOwnProperty('promoTypes')
+}
+
+function fetch_rating(id) {
+  // TODO: fetch this value from ~/tools/ratings/set_code.csv
+  return 1;
+}
+
+function fetch_latent_color(id) {
+  // TODO fetch this value from ~/tools/ratings/set_code-latent.csv
+  return null;
 }
 
 fetch_cards_for_set('M21').then((cards) => {
